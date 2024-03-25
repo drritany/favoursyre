fbe854eb4384d4674f43b3d8419740c28e67ccf3
Syre <139006607+drritany@users.noreply.github.com>
2024-03-25 08:18:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'drive.google.com'
+          },
+        //   {
+        //     protocol: 'https',
+        //     hostname: 'files.edgestore.dev'
+        //   }
+        ],
+    },
+}
 
 module.exports = nextConfig
